f3ecdac11874bca255a4c7b643c561560cd1c312
Emilio9382 <emiliopinales@icloud.com>
2025-10-13 12:51:41 -0600

    basic

---
+++ main.py
@@ -0,0 +1,35 @@
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+# sting defintions 
+name_of_dog = "rose"
+name = "Emilio" 
+favHob = "Gaming"
+
+# integer definitions 
+number_of_dog = "8"
+age = 21
+numberOne = 10
+numberTwo = 5
+yearOfBirth = 2003
+currentYear = 2025
+
+#usage of cocancnation 
+print ("I have " + number_of_dog + " dogs !")
+
+#f sting usage 
+print(f"my dogs name is {name_of_dog}")
+
+#f string with integers 
+print(f"I have {number_of_dog} dogs!")
+
+#itterations 
+print(f"I have {age + 70}!")
+
+#subtracting
+print(f"subtracting 10-5 = {numberOne - numberTwo}") 
+
+#sentence with declared variables 
+print(f"My name is {name} and my favorite hobby is {favHob}!")  
+
