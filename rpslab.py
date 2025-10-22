2521efa3fd837a60a4a78208aa862079f87cc66a
Emilio9382 <emiliopinales@icloud.com>
2025-10-21 23:12:49 -0600

    comments

--- rpslab.py
+++ rpslab.py
@@ -1,10 +1,12 @@
 
 
 print(f"Welcome to rock paper scissor")
-def start_game(): 
+def start_game():  #function to start the game -EP
+    #userINput -EP
     print(f"type r for rock, type p for paper, or type s for scissors")
     playerChoice = input("Your choice is: ")
 
+    # displays user input -EP
     if (playerChoice == "r"):
         print(f"you chose rock")
     elif (playerChoice == "p"):
@@ -12,9 +14,11 @@ def start_game():
     elif (playerChoice == "s"):
         print(f"you chose scissors")
 
+    
     import random
     # random generator
     botChoice = random.randint(1, 3)
+    #displays bot choices -EP
     print(f"Bot chose: {botChoice}")
     if (botChoice == 1):
         print(f"bot chose rock")
@@ -23,6 +27,7 @@ def start_game():
     elif (botChoice == 3):
         print(f"bot chose scissors")
 
+    #possibilties -EP
     if (playerChoice == "r" and botChoice == 1): 
         print(f"tie game")
     elif (playerChoice == "p" and botChoice == 2):
@@ -42,12 +47,14 @@ def start_game():
     elif(playerChoice == "s" and botChoice == 2):
         print(f"YOU WON!!!")
 
+    #restart -EP
     print(f"do you want to continue playing: ")
     continueGame = input("Enter y or n: ")
 
     if(continueGame == "y"):
-        start_game()
+        start_game() #restart option after first match -EP
     if(continueGame == "n"):
         print(f"Game OVer")
 
+#starts the first game than will go back to the restart line up -EP
 start_game()
