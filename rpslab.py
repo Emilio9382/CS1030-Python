c8caa8bb9de785e88328f2dde3f3345723d3ac05
Emilio9382 <emiliopinales@icloud.com>
2025-10-21 23:09:09 -0600

    more changes
    added if user wanting to play again with a function call for a call back and indented everything to accommodate it

--- rpslab.py
+++ rpslab.py
@@ -1,41 +1,53 @@
 
-print(f"type r for rock, type p for paper, or type s for scissors")
-playerChoice = input("Your choice is: ")
 
-if (playerChoice == "r"):
-    print(f"you chose rock")
-elif (playerChoice == "p"):
-    print(f"you chose paper")
-elif (playerChoice == "s"):
-    print(f"you chose scissors")
+print(f"Welcome to rock paper scissor")
+def start_game(): 
+    print(f"type r for rock, type p for paper, or type s for scissors")
+    playerChoice = input("Your choice is: ")
 
-import random
-# random generator
-botChoice = random.randint(1, 3)
-print(f"Bot chose: {botChoice}")
-if (botChoice == 1):
-    print(f"bot chose rock")
-elif (botChoice == 2):
-    print(f"bot chose paper")
-elif (botChoice == 3):
-    print(f"bot chose scissors")
+    if (playerChoice == "r"):
+        print(f"you chose rock")
+    elif (playerChoice == "p"):
+        print(f"you chose paper")
+    elif (playerChoice == "s"):
+        print(f"you chose scissors")
 
-if (playerChoice == "r" and botChoice == 1): 
-    print(f"tie game")
-elif (playerChoice == "p" and botChoice == 2):
-    print(f"tie game")
-elif(playerChoice == "s" and botChoice == 3):
-    print(f"tie game")
-elif(playerChoice == "r" and botChoice == 2):
-    print(f"you lost to a bot!")
-elif(playerChoice == "r" and botChoice == 3):
-    print(f"dont get to excited. YOu only won because it's an AI")
-elif(playerChoice == "p" and botChoice == 1):
-    print(f"look at that AI is not so smart after all")
-elif(playerChoice == "p" and botChoice == 3):
-    print(f"what-a-loser")
-elif(playerChoice == "s" and botChoice == 1):
-    print(f"CONGRATUALTIONS! YOU LOSE!")
-elif(playerChoice == "s" and botChoice == 2):
-    print(f"YOU WON!!!")
+    import random
+    # random generator
+    botChoice = random.randint(1, 3)
+    print(f"Bot chose: {botChoice}")
+    if (botChoice == 1):
+        print(f"bot chose rock")
+    elif (botChoice == 2):
+        print(f"bot chose paper")
+    elif (botChoice == 3):
+        print(f"bot chose scissors")
 
+    if (playerChoice == "r" and botChoice == 1): 
+        print(f"tie game")
+    elif (playerChoice == "p" and botChoice == 2):
+        print(f"tie game")
+    elif(playerChoice == "s" and botChoice == 3):
+        print(f"tie game")
+    elif(playerChoice == "r" and botChoice == 2):
+        print(f"you lost to a bot!")
+    elif(playerChoice == "r" and botChoice == 3):
+        print(f"dont get to excited. YOu only won because it's an AI")
+    elif(playerChoice == "p" and botChoice == 1):
+        print(f"look at that AI is not so smart after all")
+    elif(playerChoice == "p" and botChoice == 3):
+        print(f"what-a-loser")
+    elif(playerChoice == "s" and botChoice == 1):
+        print(f"CONGRATUALTIONS! YOU LOSE!")
+    elif(playerChoice == "s" and botChoice == 2):
+        print(f"YOU WON!!!")
+
+    print(f"do you want to continue playing: ")
+    continueGame = input("Enter y or n: ")
+
+    if(continueGame == "y"):
+        start_game()
+    if(continueGame == "n"):
+        print(f"Game OVer")
+
+start_game()
