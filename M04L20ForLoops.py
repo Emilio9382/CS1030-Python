9a8500de23535d27f6a5a353dc779d9a0d969313
Emilio9382 <emiliopinales@icloud.com>
2025-10-31 20:56:20 -0600

    more loops

--- M04L20ForLoops.py
+++ M04L20ForLoops.py
@@ -9,8 +9,15 @@ for count in range(-5):
         print(count)
 
 #this follows the example from above but instead it starts at 2 instead of 0 -EP
-#this will keep going for 4 iterations since it hops 4 times from 2 to 8
-#in this case 2 would start and then it would go 3,4,5 makign it 4 iterations
-#if it was -2 it would be the same process but starts and -2 instead of 2
-for count in range(-2,6): 
+#this will keep going for 4 iterations since it hops 4 times from 2 to 8 -EP
+#in this case 2 would start and then it would go 3,4,5 makign it 4 iterations =EP
+#if it was -2 it would be the same process but starts and -2 instead of 2 =EP
+#good names would be start to penultimate (2nd last) -EP
+for count in range(-2,6):
+        print(count)
+
+#for this one the range would be 0 to 10 -EP
+#starts at 0 and ends not at 10 be the penultimate of 10 -EP
+#but with a twist this one instead of increments by 1 it increments by 2 each time =EP
+for count in range(0,10,2):
         print(count)
