867997452a76dedd14e15e79d8dd96f43b3a94d3
Emilio9382 <emiliopinales@icloud.com>
2025-10-31 20:42:55 -0600

    for loop lab

---
+++ M04L20ForLoops.py
@@ -0,0 +1,9 @@
+#Looks like this bit of code will loop itself a total of 5 times -EP
+#it will start from 0 to 4 making it a total of 5 iteration 0,1,2,3,4 -EP
+#and also prints that iteration -EP
+#if changed from 5 to 10 it will iteration 10 times starting from 0 ending at 9 - EP
+#it loops it self until it hits the number we want it to hit -EP
+#if its -5 we run into an error -EP
+#count would loop itself until we hit the range -EP
+for count in range(-5):
+        print(count)
