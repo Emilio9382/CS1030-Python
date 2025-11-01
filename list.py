df817578882d8af2ba597fefd9ef82da54925b9c
Emilio9382 <emiliopinales@icloud.com>
2025-10-31 21:49:33 -0600

    list

---
+++ list.py
@@ -0,0 +1,20 @@
+#declares list -EP
+scores = [37,73,91]
+#prints the score in the 2nd spot of the list -EP
+print(scores[2])
+#displays all varaibles in list -EP
+for scores in scores:
+    print(scores)
+
+#list of food (in string) =EP
+foods = ["pizza","burgers","hot dogs"]
+#prints list =EP
+print(foods)
+#prints the 2nd spot in the list =EP
+print(foods[2])
+#prints the number of things in the list -EP
+print(len(foods))
+#prints i like ___ for everything in the list =EP
+for food in foods: 
+    print("i like", food)
+
