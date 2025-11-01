9c3eff6a8ea3d7d0f58ca88a0d3b0297ff7dee0d
Emilio9382 <emiliopinales@icloud.com>
2025-10-31 22:14:24 -0600

    finsih

--- M04L20Lists.py
+++ M04L20Lists.py
@@ -17,20 +17,38 @@ print(len(foods))
 #prints i like ___ for everything in the list =EP
 for food in foods: 
     print("i like", food)
-
+#declares list of favo hobbies -EP
 Hobbies = ["motorcycle", "car", "video games"]
+#print hobbie 0 -EP
 print(Hobbies[0])
+#prints hobbie 1 -EP
 print(Hobbies[2])
-#print(HObbies[3]) gets in error
+#print(HObbies[3]) gets in error =EP
+#reaplaces hobbie 0 with new one =EP
 Hobbies[0] = "reading"
 print(Hobbies)
+#reaplaces 2nd hobbie with 2 more -EP
 Hobbies[1:2] = ["building pcs", "editing"]
 print(Hobbies)
+#replaces hobbie hobbie 2 and 3 with one hobbie -EP
 Hobbies[1:3] = ["watching YT"]
 print(Hobbies)
+#add new hobbie at the end of list -EP
 Hobbies.append("buidling pcs")
 print(Hobbies)
+#add new hobbie to the beggining of list (0)-EP
 Hobbies.insert(0,"editing")
 print(Hobbies)
+#removes a specific hobbie -EP
 Hobbies.remove("editing")
+print(Hobbies)
+#removes one hobbie from a ceratin point on the list -EP
+Hobbies.pop(2)
+print(Hobbies)
+#deletes the list entirely -EP
+Hobbies = ["motorcycle", "car", "video games"]
+del Hobbies
+#deletes contents of list but not the list itself -EP
+Hobbies = ["motorcycle", "car", "video games"]
+Hobbies.clear()
 print(Hobbies)
