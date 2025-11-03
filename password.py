8ab6a7d0bf57ce264295edbd9db8630c9f56f0a5
Emilio9382 <emiliopinales@icloud.com>
2025-11-03 12:30:28 -0700

    password

---
+++ password.py
@@ -0,0 +1,8 @@
+import random
+
+mylist = ["a", "b", "c", "d","e","f"]
+
+userChoice = input("How many letters do you want in your password: ")
+
+for mylist in range == userChoice: 
+    print(random.choice(mylist))
